9e4a5c896111b366c5799bfbc3f027f59d95f8cc
Ramiro Detke <ramiro.fer.det@gmail.com>
2019-02-17 21:25:08 -0300

    GUI con consola para prints y seleccion archivo

---
+++ tp-final/src/castro/detke/Main.java
@@ -0,0 +1,47 @@
+package castro.detke;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("XML Petri Net Parser");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel pathLabel = new JLabel("Red: ");
+        JButton selectBtn = new JButton("Seleccionar RdP");
+        pathLabel.setSize(550,30);
+        selectBtn.setSize(50, 30);
+
+        panel.add(selectBtn);
+        panel.add(pathLabel);
+        panel.setVisible(true);
+
+        frame.add( panel, BorderLayout.NORTH );
+        JTextArea ta = new JTextArea();
+        PrintStream ps = new PrintStream(new TextAreaOutputStream(ta) );
+        System.setOut( ps );
+        System.setErr( ps );
+
+
+        frame.add( new JScrollPane( ta )  );
+
+        frame.setVisible(true);
+
+        final JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(panel);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            pathLabel.setText("Red: " + selectedFile.getName());
+            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+        }
+    }
+}
+
